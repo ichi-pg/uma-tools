9395e20e367626879690e779ba9584ce4c151ee5
ichi-pg <ichi.colors@gmail.com>
2021-04-10 22:58:11 +0900

    suspend api

--- uma/api/urls.py
+++ uma/api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('combinations/', views.combinations, name='combinations'),
-    path('girls/', views.girls, name='girls'),
+    # path('combinations/', views.combinations, name='combinations'),
+    # path('girls/', views.girls, name='girls'),
 ]
